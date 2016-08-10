51b9f95b9cfef102c4f016875baf297517b9ae5b
jboss <jboss@sonar.(none)>
2016-08-10 15:27:53 +0300

    Version 0.1

---
+++ CI_Jboss/src/main/java/com/test/Project.java
@@ -0,0 +1,23 @@
+package com.test;
+
+//dependency on Apache commons-collections
+import org.apache.commons.collections.Predicate;
+
+class Project {
+
+  public static void test() {
+    System.out.println("test");
+ System.out.println("test"); 
+System.out.println("test");
+ System.out.println("test");
+ System.out.println("test");
+System.out.println("Changed info!!!");
+  }
+  
+  public static void main(String[] args) {
+    System.out.println("Hellow MTN");
+    System.out.println("New changes!!! for java");
+    
+  }
+
+}
